e8b725930d5162f84ba2c009644523aec8e6344a
Robert Moreton <rmoreton@cradlepoint.com>
2017-12-12 10:49:46 -0700

    shell reads, prints, and exits on EOF

--- shell.c
+++ shell.c
@@ -8,5 +8,56 @@
 
 int main()
 {
-    return 0;
+    repl();
+}
+
+/*
+ * Read, evaluate, and print. This is the basic functionality.
+ */
+void repl()
+{
+    // Declare variables.
+    char line[SHELL_MAX_LINE_LENGTH];
+    char *readerr;
+
+    // Loop forever!
+    while(1) {
+        // Zero out the line. Slow, but...worth it maybe?
+        for(int i = 0; i < SHELL_MAX_LINE_LENGTH; i++) {
+            line[i] = '\0';
+        }
+
+        // Print the prompt.
+        show_prompt();
+
+        // Read a line.
+        readerr = fgets(line, SHELL_MAX_LINE_LENGTH, stdin);
+        if(NULL == readerr) {
+            // Assume EOF.
+            // TODO how do I tell if this is an error?
+            close_shell(1, 0);
+        }
+
+        // Print the line.
+        fprintf(stdout, "%s", line);
+    }
+}
+
+/*
+ * Print the prompt. Could be more advanced perhaps?
+ */
+void show_prompt(void)
+{
+    fprintf(stdout, "\033[38;5;47mrsh \033[38;5;12m$\033[00m ");
+}
+
+/*
+ * Close the shell.
+ */
+void close_shell(int print_exit, int exit_status)
+{
+    if(print_exit) {
+        fprintf(stdout, "exit\n");
+    }
+    exit(exit_status);
 }
--- shell.h
+++ shell.h
@@ -1,4 +1,13 @@
 #ifndef RMORETON_SIMPLE_SHELL
 #define RMORETON_SIMPLE_SHELL
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SHELL_MAX_LINE_LENGTH 160
+
+void repl(void);
+void show_prompt(void);
+void close_shell(int, int);
+
 #endif
