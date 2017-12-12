12ca9f4253a92e2cde05573c17a3a08ce06f8d23
Robert Moreton <rmoreton@cradlepoint.com>
2017-12-12 14:17:15 -0700

    we now tokenize the input string on spaces

--- shell.c
+++ shell.c
@@ -12,6 +12,20 @@ int main()
 }
 
 /*
+ * Print the character values of everything up the the null char.
+ */
+void printchars(char *line)
+{
+    for(int i = 0; ; i++) {
+        if(line[i] == '\0') {
+            printf("NULLCHAR\n");
+            return;
+        }
+        printf("%d ", (int)line[i]);
+    }
+}
+
+/*
  * Read, evaluate, and print. This is the basic functionality.
  */
 void repl()
@@ -38,18 +52,155 @@ void repl()
             close_shell(1, 0);
         }
 
-        // Print the line.
-        fprintf(stdout, "%s", line);
+        // Evaluate the line.
+        evaluate(line);
     }
 }
 
 /*
- * Print the prompt. Could be more advanced perhaps?
+ * Print the prompt.
  */
 void show_prompt(void)
 {
     fprintf(stdout, "\033[38;5;47mrsh \033[38;5;12m$\033[00m ");
 }
+
+/*
+ * Evaluate a line.
+ */
+void evaluate(char line[SHELL_MAX_LINE_LENGTH])
+{
+    // Declare variables.
+    char *tokens[SHELL_MAX_LINE_LENGTH];
+    int num_tokens;
+
+    // Strip the newline from the line.
+    strip_newline(line);
+
+    // Tokenize the line.
+    num_tokens = tokenize(line, tokens, SHELL_MAX_LINE_LENGTH - 1);
+
+    // Print the tokens.
+    for(int i=0; i < num_tokens; i++) {
+        printf("token: %s\n", tokens[i]);
+    }
+}
+
+/*
+ * Replace the first occurence of a newline character with a null character.
+ *
+ * Line must be null terminated!
+ */
+void strip_newline(char *line)
+{
+    while(1) {
+        if(*line == '\0' || *line == '\n') {
+            *line = '\0';
+            return;
+        }
+        line++;
+    }
+}
+
+/*
+ * Tokenize a line by spaces (ignore quotes).
+ *
+ * max_tokens must be > 0
+ */
+int tokenize(char *line, char **tokens, int max_tokens)
+{
+    // Declare variables.
+    int i;
+    char *saveptr;
+
+    // Error check.
+    if(max_tokens <= 0) {
+        fprintf(stderr, "attempted to tokenize input, but max_tokens was 0");
+        return 0;
+    }
+
+    // Loop over the line and get tokens from it.
+    for(i=0; ; i++, line=NULL) {
+        // Don't tokenize too much! If the string can't be fully tokenized,
+        // we just drop the end. That's not great.
+        // TODO don't just drop the end of the string.
+        if(i >= max_tokens-1) {
+            break;
+        }
+        // Get a new token. Break if it's NULL.
+        tokens[i] = strtok_r(line, ' ', &saveptr);
+        if(tokens[i] == NULL) {
+            break;
+        }
+    }
+
+    // Return how many tokens we read.
+    return i;
+}
+
+/*
+ * Get a token from a string.
+ *
+ * This changes first argument!
+ * The line must be null-terminated!
+ */
+char *strtok_r(char *line, char delim, char **saveptr)
+{
+    // Declare variables.
+    char *start;
+    char *end;
+
+    // Error check.
+    if(delim == '\0') {
+        fprintf(stderr, "cannot tokenize on the null character");
+        return NULL;
+    }
+
+    // Set start appropriately.
+    if(NULL != line) {
+        start = line;
+    } else if(NULL != saveptr) {
+        start = *saveptr;
+    } else {
+        fprintf(stderr, "must provide either line or saveptr");
+        return NULL;
+    }
+
+    // Move start up to the first non-delim character.
+    while(1) {
+        if(*start != delim) {
+            break;
+        }
+        start++;
+    }
+
+    // If start is on the null character, return null.
+    if(*start == '\0') {
+        *saveptr = start;
+        return NULL;
+    }
+
+    // Move end until the first delim character.
+    end = start + 1;
+    while(1) {
+        if(*end == delim || *end == '\0') {
+            break;
+        }
+        end++;
+    }
+
+    // Replace delim, set saveptr.
+    if(*end == '\0') {
+        *saveptr = end;
+    } else {
+        *end = '\0';
+        *saveptr = end + 1;
+    }
+
+    // Return the start of the token.
+    return start;
+}
+
 
 /*
  * Close the shell.
--- shell.h
+++ shell.h
@@ -3,11 +3,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SHELL_MAX_LINE_LENGTH 160
 
 void repl(void);
 void show_prompt(void);
+void evaluate(char *);
+void strip_newline(char *);
+int tokenize(char *, char **, int);
+char *strtok_r(char *, char, char **);
 void close_shell(int, int);
 
 #endif
